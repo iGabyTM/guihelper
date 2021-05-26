f1b6d28813950d21b989e23db8cae30e7dc3ae57
iGabyTM <contactgabytm@gmail.com>
2021-05-26 14:15:33 +0300

    Added `Options.addOption()` for easier option creation and `Option.Builder.arg()` to specify all properties in one method needed for an option with argument(s)
    Signed-off-by: iGabyTM <contactgabytm@gmail.com>

--- src/main/kotlin/me/gabytm/minecraft/guihelper/functions/Options.kt
+++ src/main/kotlin/me/gabytm/minecraft/guihelper/functions/Options.kt
@@ -2,11 +2,13 @@ package me.gabytm.minecraft.guihelper.functions
 
 import me.gabytm.minecraft.guihelper.items.heads.providers.HeadIdProvider.Provider
 import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import kotlin.reflect.KClass
 
 fun createHeadsOption(vararg providers: Provider): Option {
     return Option.builder("h")
         .longOpt("heads")
-        .argName(providers.joinToString(", ") { it.alias })
+        .arg(String::class, 1, providers.joinToString(", ") { it.alias })
         .desc("The method used to process player heads")
         .build()
 }
@@ -14,8 +16,28 @@ fun createHeadsOption(vararg providers: Provider): Option {
 fun createPageOption(description: String): Option {
     return Option.builder("p")
         .longOpt("page")
-        .argName("page")
-        .type(Int::class.java)
+        .arg(Int::class, 1, "page")
         .desc(description)
         .build()
+}
+
+fun Options.addOption(name: String, function: Option.Builder.() -> Unit) {
+    addOption(Option.builder(name).apply(function).build())
+}
+
+fun Options.addOption(name: Char, function: Option.Builder.() -> Unit) {
+    addOption(name.toString(), function)
+}
+
+fun Option.Builder.arg(type: Class<*>, number: Int, name: String, optional: Boolean = false): Option.Builder {
+    hasArg()
+    numberOfArgs(number)
+    argName(name)
+    optionalArg(optional)
+    type(type)
+    return this
+}
+
+fun Option.Builder.arg(type: KClass<*>, number: Int, name: String, optional: Boolean = false): Option.Builder {
+    return arg(type.java, number, name, optional)
 }

c1ee76133bd9237d9add335f9d4facff148e22bd
iGabyTM <contactgabytm@gmail.com>
2021-06-23 20:18:00 +0300

    Added license to all files

--- src/main/kotlin/me/gabytm/minecraft/guihelper/functions/Items.kt
+++ src/main/kotlin/me/gabytm/minecraft/guihelper/functions/Items.kt
@@ -16,6 +16,7 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 @file:JvmName("Items")
 
 package me.gabytm.minecraft.guihelper.functions
